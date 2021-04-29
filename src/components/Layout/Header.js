770c725337743ff787d8b4305ec2972f047ad37c
Mohit <mohit.mkj06@gmail.com>
2021-04-29 11:59:23 +0530

    update header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -43,7 +43,9 @@ export default function Header() {
             </div>
             <div className="hidden md:block">
               <div className="ml-4 flex items-center md:ml-6">
-                <p className="bg-indigo-700 text-white rounded px-2 py-1">Hi, {name}</p>
+                <p className="bg-indigo-700 text-white rounded px-2 py-1">
+                  Hi, {name}
+                </p>
 
                 <div className="ml-3 relative">
                   <div>
@@ -56,11 +58,9 @@ export default function Header() {
                       onClick={toggleUserMenu}
                     >
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                      <div className="w-8 h-8 bg-pink-600 rounded-full font-semibold flex justify-center items-center border-2">
+                        {name[0].toUpperCase()}
+                      </div>
                     </button>
                   </div>
                   {showUserMenu && (
@@ -157,51 +157,29 @@ export default function Header() {
               >
                 My Tests
               </NavLink>
-              <NavLink
+              {/* <NavLink
                 to="/candidates"
                 className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 block rounded-md text-base font-medium"
                 activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md block  text-base font-medium"
               >
                 My Candidates
-              </NavLink>
+              </NavLink> */}
             </div>
             <div className="pt-4 pb-3 border-t border-gray-700">
               <div className="flex items-center px-5">
                 <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
+                  <div className="w-8 h-8 bg-pink-600 rounded-full font-semibold flex justify-center items-center border-2">
+                    {name[0].toUpperCase()}
+                  </div>
                 </div>
                 <div className="ml-3">
                   <div className="text-base font-medium leading-none text-white">
-                    Tom Cook
-                  </div>
-                  <div className="text-sm font-medium leading-none text-gray-400">
-                    tom@example.com
+                    <p className="bg-indigo-700 text-white rounded px-2 py-1">
+                      Hi, {name}
+                    </p>
                   </div>
                 </div>
-                <button className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                  <span className="sr-only">View notifications</span>
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                    />
-                  </svg>
-                </button>
               </div>
-
               <div className="mt-3 px-2 space-y-1">
                 <a
                   href="/"
